23a505a9540b79279c958a94a17cae57f4a8e917
Shafin Mahmud Nahian <68639905+shafinnahian@users.noreply.github.com>
2023-08-05 16:34:28 +0600

    complete upper panel (full)

--- Chat-Application/src/Server.java
+++ Chat-Application/src/Server.java
@@ -31,12 +31,51 @@ public class Server extends JFrame implements ActionListener {
             }
         });
 
+        // add user logo
         ImageIcon imageUser = new ImageIcon(ClassLoader.getSystemResource("images/user2.png"));
-        Image imageUserObject = imageUser.getImage().getScaledInstance(24, 24, Image.SCALE_DEFAULT);   // creating a scaled img object
+        Image imageUserObject = imageUser.getImage().getScaledInstance(48, 48, Image.SCALE_DEFAULT);   // creating a scaled img object
         ImageIcon imageUserConverted1 = new ImageIcon(imageUserObject); // creating a scaled object
         JLabel userLogo = new JLabel(imageUserConverted1);
-        userLogo.setBounds(90, 20, 24, 24);
+        userLogo.setBounds(40, 10, 48, 48);
         panelServer.add(userLogo);
+
+        // add video logo
+        ImageIcon imageVideo = new ImageIcon(ClassLoader.getSystemResource("images/video2.png"));
+        Image imageVideoObject = imageVideo.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);   // creating a scaled img object
+        ImageIcon imageVideoConverted1 = new ImageIcon(imageVideoObject); // creating a scaled object
+        JLabel videoLogo = new JLabel(imageVideoConverted1);
+        videoLogo.setBounds(300, 17, 30, 30);
+        panelServer.add(videoLogo);
+
+        // add phone logo
+        ImageIcon imagePhone = new ImageIcon(ClassLoader.getSystemResource("images/phone3.png"));
+        Image imagePhoneObject = imagePhone.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);   // creating a scaled img object
+        ImageIcon imagePhoneConverted1 = new ImageIcon(imagePhoneObject); // creating a scaled object
+        JLabel phoneLogo = new JLabel(imagePhoneConverted1);
+        phoneLogo.setBounds(350, 17, 35, 30);
+        panelServer.add(phoneLogo);
+
+        // add menu logo
+        ImageIcon imageMenu = new ImageIcon(ClassLoader.getSystemResource("images/menu.png"));
+        Image imageMenuObject = imageMenu.getImage().getScaledInstance(10, 32, Image.SCALE_DEFAULT);   // creating a scaled img object
+        ImageIcon imageMenuConverted1 = new ImageIcon(imageMenuObject); // creating a scaled object
+        JLabel menuLogo = new JLabel(imageMenuConverted1);
+        menuLogo.setBounds(390, 17, 32, 32);
+        panelServer.add(menuLogo);
+
+        // adding user name
+        JLabel nameServer =new JLabel("ServerUser");
+        nameServer.setBounds(110, 12, 100, 18);
+        nameServer.setForeground(Color.WHITE);
+        nameServer.setFont(new Font("SAN SERIF", Font.BOLD, 18));
+        panelServer.add(nameServer);
+
+        // add user status
+        JLabel userStatus =new JLabel("Active now");
+        userStatus.setBounds(110, 35, 100, 18);
+        userStatus.setForeground(Color.WHITE);
+        userStatus.setFont(new Font("SAN SERIF", Font.BOLD, 14));
+        panelServer.add(userStatus);
 
         setSize(450, 700);
         setLocation(200, 200);
