df336c0b0560360b66ccdae306b097b54d8bdeba
Shafin Mahmud Nahian <68639905+shafinnahian@users.noreply.github.com>
2023-08-06 14:39:07 +0600

    add border of text, IRL time per texts, clear input panel per text

--- Chat-Application/src/Server.java
+++ Chat-Application/src/Server.java
@@ -1,9 +1,15 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class Server extends JFrame implements ActionListener {
 
@@ -111,9 +117,7 @@ public class Server extends JFrame implements ActionListener {
 
     public void actionPerformed (ActionEvent ae){
         String out = text1.getText();
-        JLabel output = new JLabel(out);
-        JPanel p2 = new JPanel();
-        p2.add(output);
+        JPanel p2 = formatLabel(out);
 
         textPanel.setLayout(new BorderLayout());
 
@@ -125,11 +129,54 @@ public class Server extends JFrame implements ActionListener {
 
         textPanel.add(verticalOutput, BorderLayout.PAGE_START);
 
+        text1.setText("");  // empties the input bar after each message
+
         repaint();
         invalidate();
         validate();
     }
+
+    public static JPanel formatLabel(String out){
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        JLabel output = new JLabel(out);
+        output.setFont(new Font("San Serif", Font.PLAIN, 25));
+        output.setBackground(new Color(37, 211, 102));
+        output.setOpaque(true);
+        output.setBorder(new EmptyBorder(15, 15, 15, 50));
+
+        panel.add(output);
+
+        // to get real time in messages
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
+
+        JLabel time = new JLabel();
+        time.setText(simpleDateFormat.format(cal.getTime()));
+
+        panel.add(time);
+
+        return panel;
+    }
     public static void main(String[] args) {
         new Server();
+
+//        try{
+//            ServerSocket skt = new ServerSocket(6001);
+//
+//            while (true){
+//                Socket s = skt.accept();
+//                DataInputStream din = new DataInputStream(s.getInputStream());  // takes the input of socket
+//                DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+//
+//                while (true){
+//                    String messageRead = din.readUTF(); // reads the message of DataInputStream object
+//                    JPanel panel = formatLabel()
+//                }
+//            }
+//        } catch (Exception e){
+//            e.printStackTrace();
+//        }
     }
 }
